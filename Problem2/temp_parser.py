83d57edf3c2f28636250c1b8c23db6d72a7a515e
Demo User <CEParo1@verizon.net>
2019-12-11 16:30:51 +0000

    Updated

--- Problem2/temp_parser.py
+++ Problem2/temp_parser.py
@@ -4,21 +4,21 @@ def main():
     temp = eval(sys.argv[1])
     file = open('temp.txt', 'r')
 
-    Sum = 0;
-    under = 0; 
-    Temp = 0;
+    total = 0;
+    below = 0; 
+    temps= 0;
     for line in file.read().split('\n')[2:-1]:
-        Temp += 1
-        Sum += eval(line)
+        temps += 1
+        total += eval(line)
         if eval(line) < temp:
-            under += 1
+            below += 1
 
-    avg = Sum / Temp
+    avg=Sum/Temp
 
     print('''File name of sensor temperatures: {0}
 Number of temperatures below {1}: {2}
 Average Temperature: {3}
-'''.format('temp.txt', temp, under, avg))
+'''.format('temp.txt', temp, below, avg))
 
 if __name__ == "__main__":
     main()
